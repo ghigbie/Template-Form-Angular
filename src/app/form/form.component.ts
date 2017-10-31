49cc69ab8d76da4e47479f59bb5c1c26b33afa78
George Higbie <gehigbie@gmail.com>
2017-10-31 16:33:24 +0000

    Modify functions

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -9,15 +9,26 @@ export class FormComponent implements OnInit {
   user = {
     name:'',
     email:'',
+    age: '',
     phone:''
   };
   
   users:object[] = [];
   
+  clearData(){
+    this.user.name = '';
+    this.user.email = '';
+    this.user.age = '';
+    this.user.phone = '';
+  }
+  
+  
   onSubmit(){
     this.users.push(this.user);
     console.log(this.users);
+    this.clearData();
   }
+  
   
   constructor(){}
   ngOnInit(){}
